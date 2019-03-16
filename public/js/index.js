2bf360fb7714c56f97c900e4efd241ad738c3b85
ritikSinha <ritikk910@gmail.com>
2019-03-16 17:34:37 +0530

    version ui

--- public/js/index.js
+++ public/js/index.js
@@ -8,14 +8,14 @@ socket.on('disconnect',()=>{
 
 socket.on('newMsg',(msg)=>{
  console.log('newMsg',msg);
- let li = jQuery('<li></li>');
-      li.text(`${msg.from}:${msg.text}`);
+ let li = jQuery('<li class="ui compact message"></li><br/>');
+      li.text(`${msg.from}   : ${msg.text}`);
       jQuery('#msg').append(li);
 })
 jQuery('#msg-form').on('submit',function(e){
   e.preventDefault();
   socket.emit('createMsg',{
-    from:'ritik',
+    from:'user',
     text:jQuery('[name=msg]').val()
   },function(data){
     console.log(data);
